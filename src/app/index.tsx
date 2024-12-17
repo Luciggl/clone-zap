7eb939b3cf5da9cda149764322976b80732a72b6
luciggl <lucielionascimento19@gmail.com>
2024-12-17 03:12:09 -0300

    feat: criando a navBar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,8 @@
 import {View, Text} from "react-native"
+import { NavBar } from "../components/navBar"
 
 export default function Index() {
     return (
-        <View>
-            <Text>
-                Bem vindo
-            </Text>
-        </View>
+        <NavBar/>
     )
 }
---
+++ src/components/navBar/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export function NavBar() {
+    return (
+        <View>
+            <Text>
+                Clone-Zap
+            </Text>
+        </View>
+    )
+}
